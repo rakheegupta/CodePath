01f3fd8a2db239490711730d2ab241021f5aad6c
Rakhee Gupta <rakheegupta@outlook.com>
2016-02-09 23:36:18 -0800

    NYTimes

---
+++ NYTimesArticle/app/src/main/java/com/rakhee/codepath/nytimesarticle/Article.java
@@ -0,0 +1,32 @@
+package com.rakhee.codepath.nytimesarticle;
+
+import java.util.List;
+
+/**
+ * Created by varungupta on 2/9/16.
+ */
+public class Article {
+    private String url;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public static List<Article> createArticleList(){
+
+        return null;
+    }
+}
---
+++ NYTimesArticle/app/src/main/java/com/rakhee/codepath/nytimesarticle/MainActivity.java
@@ -0,0 +1,38 @@
+package com.rakhee.codepath.nytimesarticle;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater infalter = getMenuInflater();
+        infalter.inflate(R.menu.action_search,menu);
+        MenuItem searchItem =menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
+}
