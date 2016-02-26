eccfbd6fa1d03479ca3f87397d7ba33bf1ba221c
Rakhee Gupta <Rakhee Gupta>
2016-02-25 23:02:14 -0800

    add a new user profile activity

--- SimpleTweets/app/src/main/java/com/codepath/apps/simpleTweeter/Activities/UserProfileActivity.java
+++ SimpleTweets/app/src/main/java/com/codepath/apps/simpleTweeter/Activities/UserProfileActivity.java
@@ -3,6 +3,8 @@ package com.codepath.apps.simpleTweeter.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.codepath.apps.simpleTweeter.R;
+
 public class UserProfileActivity extends AppCompatActivity {
 
     @Override
